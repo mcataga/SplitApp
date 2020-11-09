82c4810927f5e2fcf04eff8b5ab6dda1a48e4f3b
Manveer Gill <manveergg@gmail.com>
2020-11-09 14:52:59 -0500

    Working on saving user data

--- app/src/main/java/com/example/splitapp/MainActivity.java
+++ app/src/main/java/com/example/splitapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/splitapp/UserActivity.java
@@ -0,0 +1,38 @@
+package com.example.splitapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.splitapp.db.AppDatabase;
+import com.example.splitapp.db.User;
+
+public class UserActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedState){
+        super.onCreate(savedState);
+        setContentView(R.layout.fragment_first);
+
+        final EditText firstname = findViewById(R.id.firstName);
+        final EditText lastname = findViewById(R.id.lastName);
+        final EditText email = findViewById(R.id.emailAddress);
+        final EditText password = findViewById(R.id.passwordField);
+
+        Button save = findViewById(R.id.btnSave);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUser(firstname.getText().toString(), lastname.getText().toString(), email.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+
+    private void saveUser(String first, String last, String email, String pass){
+        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
+
+        User user = new User();
+        
+    }
+}
