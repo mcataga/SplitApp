0986e752d0973da140b4f19bae2d0a9e8d24c983
Manveer Gill <manveergg@gmail.com>
2020-11-09 15:14:23 -0500

    Working on saving user data

--- app/src/main/java/com/example/splitapp/FirstFragment.java
+++ app/src/main/java/com/example/splitapp/FirstFragment.java
@@ -23,5 +23,13 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.btnSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/splitapp/MainActivity.java
+++ app/src/main/java/com/example/splitapp/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
