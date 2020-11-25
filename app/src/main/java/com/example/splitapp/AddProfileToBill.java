529dc56706298d5f83453e551ab83dab9f3bd8c9
Manveer Gill <manveergg@gmail.com>
2020-11-25 01:58:48 -0500

    Added activity views

---
+++ app/src/main/java/com/example/splitapp/AddProfileToBill.java
@@ -0,0 +1,14 @@
+package com.example.splitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProfileToBill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_profile_to_bill);
+    }
+}
